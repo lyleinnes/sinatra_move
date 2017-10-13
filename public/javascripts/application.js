4d03ee305e13a9d55e4ac432115e853fcad37f29
arielse <ariel@bottledup.net>
2017-10-13 14:26:09 +1100

    styling and functionality

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,6 +7,10 @@ console.log('JS is on!')
 var toggleVanish = function (object) {
   object.classList.toggle("hide");
 };
+var toggleRoute = function (object) {
+  object.classList.toggle("path-toggle");
+};
+
 
 //===//
 // CAPTURING THE DOM ELEMENTS WE WANT TO WORK WITH //
@@ -34,6 +38,8 @@ var disruptionInfo = document.querySelector(".disruption-info")
 
 var phoneScreen = document.querySelector(".phone-screen");
 
+var pathToggle = document.querySelector(".path");
+
 var allObjects = phoneScreen.childNodes;
 
 
@@ -50,26 +56,29 @@ var somePoint = document.querySelector(".some-point");
 
 // click on point of interest one
 poiOne.addEventListener("click", function() {
-  toggleVanish(routeOne);
+
   toggleVanish(poiOneInfo);
   toggleVanish(goButton);
+  toggleRoute(pathToggle);
 
   if (poiOneInfo.classList.contains("hide")) {
     directions.classList.add("hide");
     goButton.classList.add("hide");
+    pathToggle.classList.remove("path-toggle");
   };
+  if (directions.classList.contains("hide")) {
+    poiTwo.classList.remove("hide");
+    poiThree.classList.remove("hide");
+  }
 
-  // goButton.style.display = routeOne.style.display;
-  // if (directions.style.display === "") {
-  //   directions.style.display = "none"
-  // }
 });
 
 //click on go button from point of interest one
 goButton.addEventListener("click", function() {
   toggleVanish(goButton);
   toggleVanish(directions);
-
+  toggleVanish(poiTwo);
+  toggleVanish(poiThree);
 });
 // click on disruption
 disruption.addEventListener("click", function() {
